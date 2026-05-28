3514a9689b568f77b6ea808b6297aaad6975cac2
edwardbrefo <edwardbrefo185@gmail.com>
2026-05-28 22:46:22 +0000

    made changes to files in css and js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,11 @@
-// Hamburger Menu Functionality
+document.addEventListener('DOMContentLoaded', function() {
+    const currentYearSpan = document.getElementById('currentyear');
+    const lastModifiedSpan = document.getElementById('lastModified');
+    
+    currentYearSpan.textContent = new Date().getFullYear();
+    lastModifiedSpan.textContent = document.lastModified;
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerBtn = document.getElementById('hamburgerBtn');
     const navMenu = document.getElementById('navMenu');
@@ -16,7 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-
     document.addEventListener('click', function(event) {
         const isClickInsideNav = navMenu.contains(event.target);
         const isClickInsideBtn = hamburgerBtn.contains(event.target);
@@ -27,38 +33,3 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-const DAYS = 6;
-const LIMIT = 30;
-let studentReport = [11, 42, 33, 64, 29, 37, 44];
-
-for (let i = 0; i < studentReport; i++){
-    if (studentReport[i] < 30);
-    console.log(studentReport[i]);}
-
-const parent = document.querySelector('.parent');
-const child = parent.querySelectorAll('span')
-child.forEach(child => {
-    child.style.fontstyle = 'italic';
-    child.style.color = 'blue';
-    child.style.textDecoration = 'underline';
-});
-
-const parent2 = document.querySelector('.parent2');
-const child2 = parent2.querySelectorAll('span')
-child2.forEach(child2 => { 
-    child2.addEventListener('mouseover', () => {
-        child2.style.fontstyle = 'italic';
-        child2.style.color = 'red';
-        child2.style.textDecoration = 'underline';
-    })
-
-    child2.addEventListener('mouseout', () => {
-        child2.style.fontstyle = '';
-        child2.style.color = '';
-        child2.style.textDecoration = 'none';
-    })
-})
-
-const paragraph = document.createElement('p');
-paragraph.textContent = "This is a new paragraph added by JavaScript.";
